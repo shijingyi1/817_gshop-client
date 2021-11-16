796416c1ecf656fe898491544c2e9129a276c2bc
石晶一 <1018692244@qq.com>
2021-11-16 12:39:08 +0800

    脚手架更新

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+//不显示非生产环境打包提示
 Vue.config.productionTip = false
 
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+lintOnSave: false ,//关闭ESLint的规则检查
+    //lintOnSave: 'warning',  //输入错误，但项目继续运行
+}
