0e37d4c7fddf141b669ec42d37bd2010a84c9e89
shijingyi <1018692244@qq.com>
2021-11-17 23:35:45 +0800

    11.17

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import TypeNav from './components/TypeNav'
 
 
 //不显示非生产环境打包提示
 Vue.config.productionTip = false
 
+Vue.component(TypeNav.name, TypeNav)
+
 new Vue({
-  render: h => h(App),
-  router,//注册路由器  所有组件都可以直接访问2个对象：$router(路由器对象)与$route(当前路由信息对象)
+    render: h => h(App),
+    router, //注册路由器  所有组件都可以直接访问2个对象：$router(路由器对象)与$route(当前路由信息对象)
 }).$mount('#app')
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,17 +1,41 @@
 <template>
-    <div class="home">Home</div>
+    <div class="home">
+        <TypeNav></TypeNav>
+        <ListContainer/>
+        <TodayRecommend/>
+        <Rank/>
+        <Like/>
+        <Floor/>
+        <Floor/>
+        <Brand/>
+
+    </div>
 </template>
 
 <script>
+import ListContainer from './ListContainer'
+import TodayRecommend from './TodayRecommend'
+import Rank from "./Rank/Rank.vue";
+import Like from "./Like/Like.vue";
+import Floor from "./Floor/Floor.vue";
+import Brand from "./Brand/Brand.vue";
+
 
 export default {
+  components: {
+  ListContainer,
+  TodayRecommend,
+  Rank,
+  Like,
+  Floor,
+  Brand
+
+  },
    name:'Home',
 }
 </script>
 
 <style lang="less" scoped>
-    .home {
-        color:red;
-    }
+   
 
 </style>
